6a1d5855d5f68a8f7b29b0800815dca71bdbf607
karlwang2022 <karlwang2022@gmail.com>
2025-02-08 16:25:37 -0500

    omit

---
+++ newsTemplate.py
@@ -0,0 +1,5 @@
+from functions import *
+
+print(ask_question("What is the capital of the United States?"))
+#print(request_image("A painting of a beautiful sunset over United States."))
+
---
+++ openaiTest.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+import os
+
+
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def ask_question(question):
+    completion = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": question},
+        ]
+    )
+    return completion.choices[0].message.content
+
+def request_image(prompt):
+    response = client.images.generate(
+        model = "dall-e-2",
+        prompt = prompt,
+        size = "1024x1024",
+        quality = "standard",
+        n = 1,
+    )
+    return response.data[0].url
